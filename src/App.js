c108b21c80a9e21eb566be4ed0032f735e84ea1e
mzaouchi <mzaouchi@gmail.com>
2024-04-22 12:27:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount] = useState(0)
+  const [text,setText] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Workshop HOOKS</h1>
+      <button onClick={()=> setCount(count-1)}>-</button>
+      <span>{count}</span>
+      <button onClick={()=> setCount(count+1)}>+</button>
+      <br/>
+      <br/>
+      <input onChange={(e)=> setText(e.target.value)} type='text'/>
+      <h2>{text}</h2>
     </div>
   );
 }
